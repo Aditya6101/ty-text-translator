ef1cfc816a84585871b2abbdf502d3f0af7b2eba
Aditya Kamble <adityakamble6101@gmail.com>
2022-05-08 23:35:22 +0530

    GUI init.

---
+++ GUI.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("500x500")
+
+root.title('Text Translator')
+
+heading = tk.Label(root, text='Text Translator', font=('Poppins', 20))
+heading.pack()
+
+root.mainloop()
